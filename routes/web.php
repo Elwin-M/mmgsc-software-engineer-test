4af339ccb91637f32ba47456da07df97c6b5f3a8
Elwin M <elwin-moolakkattu@live.ca>
2025-04-18 18:09:48 -0400

    added empty routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Settings;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Transactions;
 
 
-Route::get('/', Transactions::class);
+// Main route with placeholder routes for non-existant pages
+Route::get('/', Transactions::class)->name('Transactions');
+// Placecholders
+Route::get('/settings', Settings::class)->name('Settings');
+Route::get('/user-settings', Settings::class)->name('User Settings');
+Route::get('/atm-settings', Settings::class)->name('ATM Settings');
+Route::get('/my-settings', Settings::class)->name('My Settings');
