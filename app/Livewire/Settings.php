3e51e59f6a1805954be6475058e8de9af82cbdbf
Elwin M <elwin-moolakkattu@live.ca>
2025-04-18 18:08:28 -0400

    added not empty pages and removed useless page

---
+++ app/Livewire/Settings.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Settings extends Component
+{
+    // Empty page
+    public function render()
+    {
+        return view('livewire.settings');
+    }
+}
---
+++ resources/views/livewire/settings.blade.php
@@ -0,0 +1,6 @@
+<div class="h-full flex items-center justify-center">
+        {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <div class="text-center text-gray-700 text-xl">
+        Page Not Implemented
+    </div>
+</div>
